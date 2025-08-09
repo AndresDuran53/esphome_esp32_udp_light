c44b620e175b871f50b0411cba016c1a810ceeaa
Andres <andresduranrod@gmail.com>
2025-08-09 13:23:40 -0600

    Adding other parameters

--- components/empty_component/__init__.py
+++ components/empty_component/__init__.py
@@ -1,6 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.components import light
+from esphome.components.light import AddressableLightState
+from esphome.const import CONF_ID, CONF_PORT, CONF_LIGHT_ID
+
+DEPENDENCIES = ['wifi']
+AUTO_LOAD = ['light']
 
 empty_component_ns = cg.esphome_ns.namespace("empty_component")
 EmptyComponent = empty_component_ns.class_("EmptyComponent", cg.Component)
@@ -8,10 +13,15 @@ EmptyComponent = empty_component_ns.class_("EmptyComponent", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(EmptyComponent),
+        cv.Required(CONF_PORT): cv.port,
+        cv.Required(CONF_LIGHT_ID): cv.use_id(AddressableLightState),
     }
 ).extend(cv.COMPONENT_SCHEMA)
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
+    light_state = await cg.get_variable(config[CONF_LIGHT_ID])
+    cg.add(var.set_strip(light_state))
+    cg.add(var.set_port(config[CONF_PORT]))
--- components/esp32_udp_light/__init__OLD.py
+++ components/esp32_udp_light/__init__OLD.py
@@ -4,17 +4,19 @@ from esphome.components import light
 from esphome.components.light import AddressableLightState
 from esphome.const import CONF_ID, CONF_PORT, CONF_LIGHT_ID
 
-# DEPENDENCIES = ['wifi']
+DEPENDENCIES = ['wifi']
 AUTO_LOAD = ['light']
 
 esp32_udp_light_ns = cg.esphome_ns.namespace('esp32_udp_light')
 UDPStripLightComponent = esp32_udp_light_ns.class_('UDPStripLightComponent', cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(UDPStripLightComponent),
-    cv.Required(CONF_PORT): cv.port,
-    cv.Required(CONF_LIGHT_ID): cv.use_id(AddressableLightState),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(UDPStripLightComponent),
+        cv.Required(CONF_PORT): cv.port,
+        cv.Required(CONF_LIGHT_ID): cv.use_id(AddressableLightState),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
