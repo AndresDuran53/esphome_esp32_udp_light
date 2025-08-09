cb39cfe246df6e2ed7b309af9bebd75da5f24778
Andres <andresduranrod@gmail.com>
2025-08-09 15:57:53 -0600

    Nested counter

--- components/esp32_udp_light/esp32_udp_light.cpp
+++ components/esp32_udp_light/esp32_udp_light.cpp
@@ -26,10 +26,11 @@ void UDPStripLightComponent::loop() {
 
     if (this->boot_loop_counter_ < BOOT_LOOP_DELAY) {
         this->boot_loop_counter_++;
+        if (this->boot_loop_counter_ == BOOT_LOOP_DELAY) {
+            ESP_LOGI(TAG, "Boot loop counter reached %d, initializing socket", BOOT_LOOP_DELAY);
+            this->boot_loop_counter_++;
+        }
         return;
-    }
-    if (this->boot_loop_counter_ == BOOT_LOOP_DELAY) {
-        ESP_LOGI(TAG, "Boot loop counter reached %d, initializing socket", BOOT_LOOP_DELAY);
     }
 
     if (this->socket_fd_ < 0) {
